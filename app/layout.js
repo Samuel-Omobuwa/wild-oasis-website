787bf2dea5124b2ce95e9ad4c83c80f5803843f3
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-05-24 01:10:17 +0100

    added features

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,14 @@
 import Logo from "./_components/Logo";
 import "@/app/_styles/globals.css";
 
+import {Josefin_Sans} from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap"
+})
+
 export const metadata = {
  title: {
   templete: "%s / The Wild Oasis",
@@ -12,11 +20,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     <body className="bg-primary-900 text-primary-100 min-h-screen">
-     <header>
-        <Logo />
-      </header>
-      <main>{children}</main>
+     <body className={`${josefin.className} antialiased bg-primary-900 text-primary-100 min-h-screen flex flex-col`}>
+      <Header />
+      <div className="flex-1 px-8 py-12">
+      <main className="max-w-7xl mx-auto">{children}</main>
+      </div>
+      
      </body>
     </html>
   );
