2ed9ee2ae369ba7d4dcd2ca6879e1fef76c98ef3
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-05-24 23:49:49 +0100

    home page creation

--- app/page.js
+++ app/page.js
@@ -1,6 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import bgImg from '../public/bg.png';
+
 
 export default function Page() {
   return (
-    <div>Hello Next!</div>
+      <main className="mt-24">
+      <Image src={bgImg} alt="Mountains and forests with two cabins" />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
   );
 }
