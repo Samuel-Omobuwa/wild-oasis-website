5238ba616c00e11262318a7a8071fc7b6955e725
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-05-25 13:29:42 +0100

    connecting with database

---
+++ app/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://pycauoviylaxiklcuoui.supabase.co'
+const supabaseKey =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5Y2F1b3ZpeWxheGlrbGN1b3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMzI1OTgsImV4cCI6MjA2MzcwODU5OH0.Ykr8W9Ryg4toWCUbPBsdSL2r8Lp1j6elsmTv27D86RQ"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
