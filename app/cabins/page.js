a7088a9b87e523eedf3884e39c32033ebe5694a7
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-06-23 08:48:52 +0100

    added features

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,38 @@
-"use client"
-import { useEffect } from "react";
-import { getCabin } from "../services/apiCabins";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
+import { getCabins } from "../_lib/data-service";
 
-export default function Page() {
+export const revalidate = 3600;
+// export const revalidate = 15;
+
+export const metadata = {
+  title: "Cabins",
+};
+
+export default async function Page() {
+  console.log("starting...");
+
+  const cabins = await getCabins();
+  console.log(cabins);
+
   return (
     <div>
-      <h1> This is the Cabins</h1>
-      <p>Explore our luxury cabins in the mountains.</p>
-      <div></div>
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
